e731bf89f3364a7db7e63da3344abae7a3901f9e
Aydar <aidaraliev@gmail.com>
2017-07-23 18:57:53 +0600

    added aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,11 +1,10 @@
 """main module"""
 
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import functions as gf
-import numpy as np
-from pygame.sprite import Group
 
 def run_game():
     """ Initialize game and create a screen object."""
@@ -17,12 +16,15 @@ def run_game():
     ship = Ship(ai_settings, screen)
     #Make a group to store bullets in
     bullets = Group()
+    aliens = Group()
+    #Make an alien
+    gf.create_fleet(ai_settings, screen, aliens)
     # Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events.
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
         gf.update_bullets(bullets)
         print(len(bullets))
 run_game()
--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from bullet import Bullet
+from alien import Alien
 
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
@@ -10,6 +11,8 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         fire_bullet(ai_settings, screen, ship, bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
 
 def fire_bullet(ai_settings, screen, ship, bullets):
     """Fire a bullet if limit not reached yet"""
@@ -17,7 +20,6 @@ def fire_bullet(ai_settings, screen, ship, bullets):
     if len(bullets) < ai_settings.bullets_allowed:
         new_bullet = Bullet(ai_settings, screen, ship)
         bullets.add(new_bullet)
-    
 
 def check_keyup_events(event, ship):
     if event.key == pygame.K_RIGHT:
@@ -35,13 +37,30 @@ def check_events(ai_settings, screen, ship, bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship, bullets):
+def create_fleet(ai_settings, screen, aliens):
+    """Create a full fleet of aliens."""
+    # Create an alien and find the number of aliens in a row.
+    # Spacing between each alien is equal to one alien width.
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    # Create the first row of aliens.
+    for alien_number in range(number_aliens_x):
+        # Create an alien and place it in the row.
+        alien = Alien(ai_settings, screen)
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        aliens.add(alien)
+
+def update_screen(ai_settings, screen, ship, aliens, bullets):
     #Redraw the screen during each pass through the loop
     screen.fill(ai_settings.bg_color)
     #Redraw all bullets behind ship and aliens
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
+    aliens.draw(screen)
     # Make the most recently drawn screen visible.
     pygame.display.flip()
 
